dea51758fc0344b5f75da9aaa0d44b1f763befef
Anshul Pratap Singh <anshulpratapsingh10@gmail.com>
2024-02-13 16:05:08 +0530

    created user auth controller and tested saving data to mongodb

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import UserRoute from './routes/user.routes.js';
+import AuthRoute from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -13,9 +14,10 @@ mongoose.connect(process.env.MONGO)
     });
 
 const app = express();
-
+app.use(express.json());
 app.listen(3000,()=>{
     console.log('server is running');
 });
 
 app.use('/api/user',UserRoute);
+app.use('/api/auth',AuthRoute);
