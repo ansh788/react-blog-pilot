1f27573a0a228af9f2a35771f1994c913cdb71d3
Anshul Pratap Singh <anshulpratapsingh10@gmail.com>
2024-02-20 11:39:00 +0530

    completed signin functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -86,7 +86,7 @@ try{
         </form>
         <div className='flex gap-2 text-sm mt-5'>
           <span>Have an account?</span>
-          <Link to='sign-in' className='text-blue-500'>Sign In</Link>
+          <Link to='/signin' className='text-blue-500'>Sign In</Link>
         </div>
         {
           errorMessage &&
