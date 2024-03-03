1d8882ec5cccf362f58493c07aec282785cf0069
Anshul Pratap Singh <anshulpratapsingh10@gmail.com>
2024-03-03 15:59:53 +0530

    added Google OAuth functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -3,6 +3,7 @@ import {Link, useNavigate} from 'react-router-dom';
 import {Alert, Button, Label, Spinner, TextInput} from 'flowbite-react';
 import {useDispatch, useSelector} from 'react-redux';
 import {signInStart, signInSuccess, signInFailure} from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 export default function Signin() {
   const [formData, setFormData]= useState({});
@@ -78,7 +79,7 @@ dispatch(signInStart());
             
           }
         </Button>
-
+          <OAuth/>
         </form>
         <div className='flex gap-2 text-sm mt-5'>
           <span>Don't Have an account?</span>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import {Alert, Button, Label, Spinner, TextInput} from 'flowbite-react';
+import OAuth from '../components/OAuth';
 
 export default function Signup() {
   const [formData, setFormData]= useState({});
@@ -82,7 +83,7 @@ try{
             
           }
         </Button>
-
+          <OAuth/>
         </form>
         <div className='flex gap-2 text-sm mt-5'>
           <span>Have an account?</span>
